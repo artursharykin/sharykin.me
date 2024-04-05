968d4fbe4b8ac05516abcfb79cf2abb7bcf6c98a
Artur Sharykin <artur.sharykin@wfsinc.com>
2024-04-05 16:03:14 -0600

    Toggle theme settings now locally stored -Caches settings to allow settings to be kept between pages -Fixed certain elements not properly inheriting colors, manually set -Added classes for paragraphs to easier format

--- theme-toggle.js
+++ theme-toggle.js
@@ -1,18 +1,36 @@
-document.addEventListener('DOMContentLoaded', (event) => {
+document.addEventListener('DOMContentLoaded', () => {
     const themeToggleButton = document.getElementById('theme-toggle');
-    const currentThemeIcon = themeToggleButton.firstElementChild;
+    const currentTheme = localStorage.getItem('theme');
+
+    if (currentTheme) {
+        document.body.classList.add(currentTheme);
+        updateIcon(currentTheme); 
+    }
 
     themeToggleButton.addEventListener('click', () => {
-        document.body.classList.toggle('light-mode');
-        document.body.classList.toggle('dark-mode');
-
-        if (document.body.classList.contains('light-mode')){
-            currentThemeIcon.classList.remove('fa-sun');
-            currentThemeIcon.classList.add('fa-moon');
-        }
-        else{
-            currentThemeIcon.classList.remove('fa-moon');
-            currentThemeIcon.classList.add('fa-sun');
+        if (document.body.classList.contains('light-mode')) {
+            document.body.classList.replace('light-mode', 'dark-mode');
+            localStorage.setItem('theme', 'dark-mode');
+            updateIcon('dark-mode');
+        } else {
+            document.body.classList.replace('dark-mode', 'light-mode');
+            localStorage.setItem('theme', 'light-mode');
+            updateIcon('light-mode');
         }
     });
 });
+
+function updateIcon(theme) {
+    const themeToggleButton = document.getElementById('theme-toggle');
+    const currentThemeIcon = themeToggleButton.firstElementChild;
+
+    if (theme === 'light-mode') {
+        currentThemeIcon.classList.remove('fa-sun');
+        currentThemeIcon.classList.add('fa-moon');
+        currentThemeIcon.style.color = '#5D3B76';
+    } else {
+        currentThemeIcon.classList.remove('fa-moon');
+        currentThemeIcon.classList.add('fa-sun');
+        currentThemeIcon.style.color = '#FFD700';
+    }
+}
